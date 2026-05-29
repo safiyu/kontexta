5983368fcc381dd604275ed1f034d410cd8058c8
safiyu <dheensafiyu@gmail.com>
2026-05-30 01:39:22 +0200

    feat(core/db): SQLite schema + FTS5 setup

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,4 @@
 export { assertPathInside, withLock } from "./util/safety.js";
 export { estimateTokensFromBuffer, estimateTokensFromString } from "./util/tokens.js";
 export type * from "./types.js";
+export { createDatabase, getDatabase, closeDatabase } from "./db/index.js";
